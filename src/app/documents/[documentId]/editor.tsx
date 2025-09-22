5be621ad0667c1f71298ea56a8d7bc08cce125c1
ruslansymonenko <symonenkors@gmail.com>
2025-09-22 19:45:09 +0300

    improved editor functional and styling

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Table from "@tiptap/extension-table";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 import StarterKit from "@tiptap/starter-kit";
 
 export const Editor = () => {
@@ -12,7 +20,17 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border-[#C7C7C7] border flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({ nested: true }),
+      TableCell,
+      TableHeader,
+      TableRow,
+      Table,
+      Image,
+      ImageResize,
+    ],
     content: "<p>Hello World!</p>",
   });
 
